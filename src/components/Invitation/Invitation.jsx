df17955986cb1f7980f51f228eac69e2f6829ffa
staszad <stas_ady@protonmail.com>
2022-07-01 20:08:51 +0200

    connected to express backend, fetch finally works mf

--- src/components/Invitation/Invitation.jsx
+++ src/components/Invitation/Invitation.jsx
@@ -1,15 +1,27 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const Invitation = () => {
+  const [initialState, setInitialState] = useState([]);
   let { personID } = useParams();
   console.log(personID);
+  const api = "http://localhost:8000/" + personID;
+  console.log(api);
 
-  async function fecthData() {
-    const response = await fetch("localhost:8000/" + personID);
-    console.log(response);
-  }
+  useEffect(() => {
+    fetch(api)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((jsonResponse) => {
+        setInitialState(jsonResponse);
+        console.log(jsonResponse);
+        const person = jsonResponse.data.result;
+        console.log(person);
+      });
+  }, []);
 
-  fecthData();
-
-  return <div>{personID}</div>;
+  return <div>heil hitler</div>;
 };
